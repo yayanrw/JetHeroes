4feca6cc02baafad1a4e437110dfba6c4c94f87d
yayanrw <yayanraw@gmail.com>
2022-11-08 15:13:15 +0700

    add model

---
+++ app/src/main/java/com/heyproject/jetheroes/model/Hero.kt
@@ -0,0 +1,10 @@
+package com.heyproject.jetheroes.model
+
+/**
+Written by Yayan Rahmat Wijaya on 11/8/2022 15:11
+Github : https://github.com/yayanrw
+ **/
+
+data class Hero(
+    val id: String, val name: String, val photoUrl: String
+)
