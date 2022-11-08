46248f24cf6ece5039b10b3245c60e0249cc1ca0
yayanrw <yayanraw@gmail.com>
2022-11-08 15:54:36 +0700

    add button to up

--- app/src/main/java/com/heyproject/jetheroes/JetHeroesApp.kt
+++ app/src/main/java/com/heyproject/jetheroes/JetHeroesApp.kt
@@ -1,15 +1,25 @@
 package com.heyproject.jetheroes
 
+import androidx.compose.animation.*
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.lazy.rememberLazyListState
 import androidx.compose.foundation.shape.CircleShape
-import androidx.compose.material.Text
+import androidx.compose.material.*
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.KeyboardArrowUp
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.derivedStateOf
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
+import androidx.compose.ui.draw.shadow
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
@@ -17,6 +27,8 @@ import androidx.compose.ui.unit.dp
 import coil.compose.AsyncImage
 import com.heyproject.jetheroes.model.HeroesData
 import com.heyproject.jetheroes.ui.theme.JetHeroesTheme
+import kotlinx.coroutines.launch
+
 
 /**
 Written by Yayan Rahmat Wijaya on 11/8/2022 15:13
@@ -28,12 +40,34 @@ fun JetHeroesApp(
     modifier: Modifier = Modifier,
 ) {
     Box(modifier = modifier) {
-        LazyColumn {
+        val scope = rememberCoroutineScope()
+        val listState = rememberLazyListState()
+        val showButton: Boolean by remember {
+            derivedStateOf { listState.firstVisibleItemIndex > 0 }
+        }
+
+        LazyColumn(
+            state = listState, contentPadding = PaddingValues(bottom = 80.dp)
+        ) {
             items(HeroesData.heroes, key = { it.id }) { hero ->
                 HeroListItem(
                     name = hero.name, photoUrl = hero.photoUrl, modifier = Modifier.fillMaxWidth()
                 )
             }
+        }
+        AnimatedVisibility(
+            visible = showButton,
+            enter = fadeIn() + slideInVertically(),
+            exit = fadeOut() + slideOutVertically(),
+            modifier = Modifier
+                .padding(bottom = 30.dp)
+                .align(Alignment.BottomCenter)
+        ) {
+            ScrollToTopButton(onClick = {
+                scope.launch {
+                    listState.scrollToItem(index = 0)
+                }
+            })
         }
     }
 }
@@ -63,12 +97,31 @@ fun HeroListItem(
     }
 }
 
+@Composable
+fun ScrollToTopButton(
+    onClick: () -> Unit, modifier: Modifier = Modifier
+) {
+    Button(
+        onClick = onClick,
+        modifier = modifier
+            .shadow(elevation = 10.dp, shape = CircleShape)
+            .clip(shape = CircleShape)
+            .size(56.dp),
+        colors = ButtonDefaults.buttonColors(
+            backgroundColor = Color.White, contentColor = MaterialTheme.colors.primary
+        )
+    ) {
+        Icon(
+            imageVector = Icons.Filled.KeyboardArrowUp,
+            contentDescription = null,
+        )
+    }
+}
+
 @Preview(showBackground = true)
 @Composable
-fun JetHeroesAppPreview() {
+fun DefaultPreview() {
     JetHeroesTheme {
-        HeroListItem(
-            name = "H.O.S. Cokroaminoto", photoUrl = ""
-        )
+        JetHeroesApp()
     }
 }
