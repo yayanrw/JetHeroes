d73cbca3a743172d512933a01d7a87422fa34217
yayanrw <yayanraw@gmail.com>
2022-11-08 15:15:55 +0700

    cleanup MainActivity.kt

--- app/src/main/java/com/heyproject/jetheroes/MainActivity.kt
+++ app/src/main/java/com/heyproject/jetheroes/MainActivity.kt
@@ -6,10 +6,7 @@ import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
-import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
 import com.heyproject.jetheroes.ui.theme.JetHeroesTheme
 
 class MainActivity : ComponentActivity() {
@@ -19,25 +16,11 @@ class MainActivity : ComponentActivity() {
             JetHeroesTheme {
                 // A surface container using the 'background' color from the theme
                 Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colors.background
+                    modifier = Modifier.fillMaxSize(), color = MaterialTheme.colors.background
                 ) {
-                    Greeting("Android")
+                    JetHeroesApp()
                 }
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
-}
-
-@Preview(showBackground = true)
-@Composable
-fun DefaultPreview() {
-    JetHeroesTheme {
-        Greeting("Android")
     }
 }
