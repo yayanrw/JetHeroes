9aaa06ce42f59ed06cc10d0fa2a603387e12bda5
yayanrw <yayanraw@gmail.com>
2022-11-09 21:30:29 +0700

    using vm

--- app/src/main/java/com/heyproject/jetheroes/JetHeroesApp.kt
+++ app/src/main/java/com/heyproject/jetheroes/JetHeroesApp.kt
@@ -11,11 +11,7 @@ import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material.*
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.KeyboardArrowUp
-import androidx.compose.runtime.Composable
-import androidx.compose.runtime.derivedStateOf
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
@@ -26,8 +22,9 @@ import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.lifecycle.viewmodel.compose.viewModel
 import coil.compose.AsyncImage
-import com.heyproject.jetheroes.model.HeroesData
+import com.heyproject.jetheroes.data.HeroRepository
 import com.heyproject.jetheroes.ui.theme.JetHeroesTheme
 import kotlinx.coroutines.launch
 
@@ -40,9 +37,13 @@ Github : https://github.com/yayanrw
 @OptIn(ExperimentalFoundationApi::class)
 @Composable
 fun JetHeroesApp(
-    modifier: Modifier = Modifier,
+    modifier: Modifier = Modifier, viewModel: JetHeroesViewModel = viewModel(
+        factory = ViewModelFactory(
+            HeroRepository()
+        )
+    )
 ) {
-    val groupedHeroes = HeroesData.heroes.sortedBy { it.name }.groupBy { it.name[0] }
+    val groupedHeroes by viewModel.groupedHeroes.collectAsState()
     Box(modifier = modifier) {
         val scope = rememberCoroutineScope()
         val listState = rememberLazyListState()
