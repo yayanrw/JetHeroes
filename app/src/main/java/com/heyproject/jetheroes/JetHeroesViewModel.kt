3f04639a4b0dd9750afe3c9d87581f252d8f9950
yayanrw <yayanraw@gmail.com>
2022-11-09 21:31:57 +0700

    add method on vm

--- app/src/main/java/com/heyproject/jetheroes/JetHeroesViewModel.kt
+++ app/src/main/java/com/heyproject/jetheroes/JetHeroesViewModel.kt
@@ -1,5 +1,7 @@
 package com.heyproject.jetheroes
 
+import androidx.compose.runtime.State
+import androidx.compose.runtime.mutableStateOf
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProvider
 import com.heyproject.jetheroes.data.HeroRepository
@@ -16,6 +18,15 @@ class JetHeroesViewModel(private val repository: HeroRepository) : ViewModel() {
     private val _groupedHeroes =
         MutableStateFlow(repository.getHeroes().sortedBy { it.name }.groupBy { it.name[0] })
     val groupedHeroes: StateFlow<Map<Char, List<Hero>>> get() = _groupedHeroes
+
+    private val _query = mutableStateOf("")
+    val query: State<String> get() = _query
+
+    fun search(newQuery: String) {
+        _query.value = newQuery
+        _groupedHeroes.value =
+            repository.searchHeroes(_query.value).sortedBy { it.name }.groupBy { it.name[0] }
+    }
 }
 
 class ViewModelFactory(private val repository: HeroRepository) :
